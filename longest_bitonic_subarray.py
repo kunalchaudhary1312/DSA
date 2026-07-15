f4efa9e69e3fdcfc03188dafd5a9ff6b85e96cc6
Kunal Chaudhary <kunal13122002@gmail.com>
2026-07-15 16:06:31 +0530

    Implement function to find longest bitonic subarray

---
+++ longest_bitonic_subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def bitonic(self, arr):
+        n = len(arr)
+        if n == 0: 
+            return 0
+            
+        inc = [1] * n
+        for i in range(1, n):
+            if arr[i] >= arr[i-1]:
+                inc[i] = inc[i-1] + 1
+                
+        dec = [1] * n
+        for i in range(n-2, -1, -1):
+            if arr[i] >= arr[i+1]:
+                dec[i] = dec[i+1] + 1
+                
+        max_len = 0
+        for i in range(n):
+            max_len = max(max_len, inc[i] + dec[i] - 1)
+            
+        return max_len 
